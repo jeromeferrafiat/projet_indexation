bc656ea698861a1c867a5e6db02e010e873c3144
baillema <mathieu.baille@gmail.com>
2018-02-28 12:21:58 +0100

    Add generic writting file

--- read_image.c
+++ read_image.c
@@ -25,11 +25,49 @@ int check(float value){
   return palier;
 }
 
+char *remove_ext (char* mystr, char dot, char sep) {
+    char *retstr, *lastdot, *lastsep;
+
+    if (mystr == NULL)
+        return NULL;
+    if ((retstr = malloc (strlen (mystr) + 1)) == NULL)
+        return NULL;
+
+    strcpy (retstr, mystr);
+    lastdot = strrchr (retstr, dot);
+    lastsep = (sep == 0) ? NULL : strrchr (retstr, sep);
+
+    if (lastdot != NULL) {
+        if (lastsep != NULL) {
+            if (lastsep < lastdot) {
+              *lastdot = '\0';
+            }
+        } else {
+          *lastdot = '\0';
+        }
+    }
+    return retstr;
+}
+
 int main(int argc, char *argv[])
 {
   int n,nx,ny,nb;
   CIMAGE cim;
 
+  char name[80];
+  strcpy(name, "out/");
+  strcat(name, remove_ext(argv[1], '.', '/'));
+  strcat(name, "_out");
+
+  FILE *f = fopen(name, "w");
+  if (f == NULL){
+    printf("Error opening file!\n");
+    exit(1);
+  }
+
+  /*------------------------------------------------*/
+  /* initialisation du tableau hist                 */
+  /*------------------------------------------------*/
   double hist[BINS][BINS][BINS];
   for(int i = 0; i<BINS; i++){
     for(int j = 0; j<BINS; j++){
@@ -38,21 +76,24 @@ int main(int argc, char *argv[])
       }
     }
   }
+
   /*------------------------------------------------*/
-  /* lecture d'une image requ�te                    */
+  /* lecture d'une image requête                    */
   /*------------------------------------------------*/
   read_cimage(argv[1],&cim);
-  /*------------------------------------------------*/
-  /* affichage des valeurs pour le premier bloc 8x8 */
-  /* comme exemple de traitement                    */
-  /*------------------------------------------------*/
 
+  /*------------------------------------------------*/
+  /* calcul des histogrammes                        */
+  /*------------------------------------------------*/
   for(int j = 0; j<cim.ny; j++){
     for(int i = 0; i<cim.nx; i++){
       (hist[check(cim.r[i][j])][check(cim.g[i][j])][check(cim.b[i][j])])++;
     }
   }
 
+  /*------------------------------------------------*/
+  /* tranformation en proportion                    */
+  /*------------------------------------------------*/
   for(int k = 0; k<BINS; k++){
     for(int j = 0; j<BINS; j++){
       for(int i = 0; i<BINS; i++){
@@ -64,14 +105,20 @@ int main(int argc, char *argv[])
     //printf("\n");
   }
 
+  /*------------------------------------------------*/
+  /* sauvegarde sous le format libsvm               */
+  /*------------------------------------------------*/
   printf("0 ");
   for(int i = 0; i<BINS; i++){
     for(int j = 0; j<BINS; j++){
       for(int k = 0; k<BINS; k++){
-        printf("%d : %4f ",i*16+j*4+(k+1), hist[i][j][k]);
+        //printf("%d : %4f ",i*16+j*4+(k+1), hist[i][j][k]);
+        fprintf(f,"%d : %4f ",i*16+j*4+(k+1), hist[i][j][k]);
       }
     }
   }
+
+
 
   exit(0);
 }
