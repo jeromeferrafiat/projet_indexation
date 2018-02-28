60349675e7480467d1c8530ba9e7889c2277692a
jeromeferrafiat <jeromeferrafiat@aol.com>
2018-02-28 10:25:14 +0100

    resolution conflict du 14 fevrier

--- read_image.c
+++ read_image.c
@@ -3,27 +3,51 @@
 #include <stdlib.h>
 #include <string.h>
 #include <strings.h>
+
 #include "rdjpeg.h"
 
 #define BINS 4
-#define SEUIL 255/BINS
 
+int check(float value){
+  float seuil = 255 / BINS;
+  float palier = 0;
+  int done = 0;
+/*  do{
+    if(value <= seuil*(palier+1))
+      done = 1;
+    else
+      palier++;
+  }while(done != 1);
+  */
 
-//fonction qui détermine dans quel BINS est la valeur
-int categorise(int entree)
-{
-  printf("%d\n",entree);
-  printf("%d\n",SEUIL);
- // int t = entree/SEUIL;
-  return 0;//t;
+  while(done != 1){
+    if(value <= seuil*(palier+1))
+    {
+      done = 1;
+    }
+    else
+    {
+      palier++;
+    }
+  }
+
+  return palier;
 }
 
 int main(int argc, char *argv[])
 {
   int i,j,n,nx,ny,nb;
-  //histogramme
-  double h[BINS][BINS][BINS];
   CIMAGE cim;
+
+  double hist[BINS][BINS][BINS];
+  for(i = 0; i<BINS; i++){
+    for(j = 0; j<BINS; j++){
+      for(int k = 0; k<BINS; k++){
+        hist[i][j][k] = 0;
+        //printf("%lf\n", hist[i][j][k]);
+      }
+    }
+  }
   /*------------------------------------------------*/
   /* lecture d'une image requête                    */
   /*------------------------------------------------*/
@@ -32,45 +56,54 @@ int main(int argc, char *argv[])
   /* affichage des valeurs pour le premier bloc 8x8 */
   /* comme exemple de traitement                    */
   /*------------------------------------------------*/
-  printf("Largeur de l'image : %d\n",cim.nx);
-  printf("Heuteur de l'image : %d\n",cim.ny);
+  
+  // printf("Largeur de l'image : %d\n",cim.nx);
+  // printf("Heuteur de l'image : %d\n",cim.ny);
+  // printf("Plan rouge du premier bloc 8x8 :\n");
+  // for (j = 0; j < 8; j++) {       /* ligne par ligne */
+  //   printf("  ");
+  //   for (i = 0; i < 8; i++) {   /* pixel par pixel */
+  //     printf("%4d",cim.r[i][j]);
 
-  printf("Plan rouge du premier bloc 8x8 :\n");
-  for (j = 0; j < 8; j++) {       /* ligne par ligne */
-    printf("  ");
-    for (i = 0; i < 8; i++) {   /* pixel par pixel */
-      printf("%4d",cim.r[i][j]);
-    }
-    printf("\n");
-  }
-  printf("Plan vert du premier bloc 8x8 :\n");
-  for (j = 0; j < 8; j++) {       /* ligne par ligne */
-    printf("  ");
-    for (i = 0; i < 8; i++) {   /* pixel par pixel */
-      printf("%4d",cim.g[i][j]);
-    }
-    printf("\n");
-  }
-  printf("Plan bleu du premier bloc 8x8 :\n");
-  for (j = 0; j < 8; j++) {       /* ligne par ligne */
-    printf("  ");
-    for (i = 0; i < 8; i++) {   /* pixel par pixel */
-      printf("%4d",cim.b[i][j]);
-    }
-    printf("\n");
-  }
+  //   }
+  //   printf("\n");
+  // }
+  // printf("Plan vert du premier bloc 8x8 :\n");
+  // for (j = 0; j < 8; j++) {       /* ligne par ligne */
+  //   printf("  ");
+  //   for (i = 0; i < 8; i++) {   /* pixel par pixel */
+  //     printf("%4d",cim.g[i][j]);
+  //   }
+  //   printf("\n");
+  // }
+  // printf("Plan bleu du premier bloc 8x8 :\n");
+  // for (j = 0; j < 8; j++) {       /* ligne par ligne */
+  //   printf("  ");
+  //   for (i = 0; i < 8; i++) {   /* pixel par pixel */
+  //     printf("%4d",cim.b[i][j]);
+  //   }
+  //   printf("\n");
+  // }
   /*------------------------------------------------*/
 
-  for ( j = 0; j < 8; j++){
-    printf(" ");
-    for (i = 0; i < 8; i++){
-      int cat_r = categorise(cim.r[i][j]);
-      int cat_g = categorise(cim.g[i][j]);
-      int cat_b = categorise(cim.b[i][j]);
-      printf("%d %d %d\n",cat_r,cat_g,cat_b);
+  for(j = 0; j<cim.ny; j++){
+    for(i = 0; i<cim.nx; i++){
+      //printf("rouge : %d\n",check(cim.r[i][j]) );
+      //printf("vert : %d\n",check(cim.g[i][j]) );
+      //printf("bleu :%d\n\n",check(cim.b[i][j]) );
+      (hist[check(cim.r[i][j])][check(cim.g[i][j])][check(cim.b[i][j])])++;
     }
   }
 
-
+  for(int k = 0; k<BINS; k++){
+    for(j = 0; j<BINS; j++){
+      for(i = 0; i<BINS; i++){
+        hist[i][j][k] = hist[i][j][k] / (cim.ny*cim.nx);
+        printf("%4lf ", hist[i][j][k]);
+      }
+      printf("\n");
+    }
+    printf("\n");
+  }
   exit(0);
 }
