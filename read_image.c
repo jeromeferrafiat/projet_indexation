7f886be1bcd4f3a109d2a3fd0bdc6970e2ae0edb
baillema <mathieu.baille@gmail.com>
2018-02-28 10:58:33 +0100

    Update read_image.c with libsvm format

--- read_image.c
+++ read_image.c
@@ -17,7 +17,7 @@ int check(float value){
     if(value <= seuil*(palier+1)) {
       done = 1;
     }
-    elsec{
+    else{
       palier++;
     }
   }
@@ -27,12 +27,12 @@ int check(float value){
 
 int main(int argc, char *argv[])
 {
-  int i,j,n,nx,ny,nb;
+  int n,nx,ny,nb;
   CIMAGE cim;
 
   double hist[BINS][BINS][BINS];
-  for(i = 0; i<BINS; i++){
-    for(j = 0; j<BINS; j++){
+  for(int i = 0; i<BINS; i++){
+    for(int j = 0; j<BINS; j++){
       for(int k = 0; k<BINS; k++){
         hist[i][j][k] = 0;
       }
@@ -47,24 +47,31 @@ int main(int argc, char *argv[])
   /* comme exemple de traitement                    */
   /*------------------------------------------------*/
 
-  for(j = 0; j<cim.ny; j++){
-    for(i = 0; i<cim.nx; i++){
+  for(int j = 0; j<cim.ny; j++){
+    for(int i = 0; i<cim.nx; i++){
       (hist[check(cim.r[i][j])][check(cim.g[i][j])][check(cim.b[i][j])])++;
     }
   }
 
   for(int k = 0; k<BINS; k++){
-    for(j = 0; j<BINS; j++){
-      for(i = 0; i<BINS; i++){
+    for(int j = 0; j<BINS; j++){
+      for(int i = 0; i<BINS; i++){
         hist[i][j][k] = hist[i][j][k] / (cim.ny*cim.nx);
-        printf("%4lf ", hist[i][j][k]);
+        //printf("%4lf ", hist[i][j][k]);
       }
-      printf("\n");
+      //printf("\n");
     }
-    printf("\n");
+    //printf("\n");
   }
 
-  
+  printf("0 ");
+  for(int i = 0; i<BINS; i++){
+    for(int j = 0; j<BINS; j++){
+      for(int k = 0; k<BINS; k++){
+        printf("%d : %4f ",i*16+j*4+(k+1), hist[i][j][k]);
+      }
+    }
+  }
 
   exit(0);
 }
