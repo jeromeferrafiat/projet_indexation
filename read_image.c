a985d1d2d3df7142aa7240414820cb2b04e4e960
baillema <mathieu.baille@gmail.com>
2018-02-28 10:39:53 +0100

    read_image.c

--- read_image.c
+++ read_image.c
@@ -12,21 +12,12 @@ int check(float value){
   float seuil = 255 / BINS;
   float palier = 0;
   int done = 0;
-/*  do{
-    if(value <= seuil*(palier+1))
-      done = 1;
-    else
-      palier++;
-  }while(done != 1);
-  */
 
   while(done != 1){
-    if(value <= seuil*(palier+1))
-    {
+    if(value <= seuil*(palier+1)) {
       done = 1;
     }
-    else
-    {
+    elsec{
       palier++;
     }
   }
@@ -44,53 +35,20 @@ int main(int argc, char *argv[])
     for(j = 0; j<BINS; j++){
       for(int k = 0; k<BINS; k++){
         hist[i][j][k] = 0;
-        //printf("%lf\n", hist[i][j][k]);
       }
     }
   }
   /*------------------------------------------------*/
-  /* lecture d'une image requête                    */
+  /* lecture d'une image requï¿½te                    */
   /*------------------------------------------------*/
   read_cimage(argv[1],&cim);
   /*------------------------------------------------*/
   /* affichage des valeurs pour le premier bloc 8x8 */
   /* comme exemple de traitement                    */
   /*------------------------------------------------*/
-  
-  // printf("Largeur de l'image : %d\n",cim.nx);
-  // printf("Heuteur de l'image : %d\n",cim.ny);
-  // printf("Plan rouge du premier bloc 8x8 :\n");
-  // for (j = 0; j < 8; j++) {       /* ligne par ligne */
-  //   printf("  ");
-  //   for (i = 0; i < 8; i++) {   /* pixel par pixel */
-  //     printf("%4d",cim.r[i][j]);
-
-  //   }
-  //   printf("\n");
-  // }
-  // printf("Plan vert du premier bloc 8x8 :\n");
-  // for (j = 0; j < 8; j++) {       /* ligne par ligne */
-  //   printf("  ");
-  //   for (i = 0; i < 8; i++) {   /* pixel par pixel */
-  //     printf("%4d",cim.g[i][j]);
-  //   }
-  //   printf("\n");
-  // }
-  // printf("Plan bleu du premier bloc 8x8 :\n");
-  // for (j = 0; j < 8; j++) {       /* ligne par ligne */
-  //   printf("  ");
-  //   for (i = 0; i < 8; i++) {   /* pixel par pixel */
-  //     printf("%4d",cim.b[i][j]);
-  //   }
-  //   printf("\n");
-  // }
-  /*------------------------------------------------*/
 
   for(j = 0; j<cim.ny; j++){
     for(i = 0; i<cim.nx; i++){
-      //printf("rouge : %d\n",check(cim.r[i][j]) );
-      //printf("vert : %d\n",check(cim.g[i][j]) );
-      //printf("bleu :%d\n\n",check(cim.b[i][j]) );
       (hist[check(cim.r[i][j])][check(cim.g[i][j])][check(cim.b[i][j])])++;
     }
   }
@@ -105,5 +63,8 @@ int main(int argc, char *argv[])
     }
     printf("\n");
   }
+
+  
+
   exit(0);
 }
