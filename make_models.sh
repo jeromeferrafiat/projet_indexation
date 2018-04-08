dd91cac1bdae8a7030a5ad42f257f459daf3b262
dappm <maxime.dapp@etu.univ-grenoble-alpes.fr>
2018-04-08 17:52:58 +0200

    Added optimization option to make_models.sh

--- make_models.sh
+++ make_models.sh
@@ -8,5 +8,5 @@ concepts=`cat concepts.txt`
 for concept in $concepts
 do
     echo $concept
-    ../libsvm-3.22/svm-train -w+1 19 -g 1.0 -b 1 train/colorAnn/colors_$concept.svm train/model/colors_$concept.model
+    ../libsvm-3.22/svm-train -w+1 19 -g 1.0 -b 1 -h 0 train/colorAnn/colors_$concept.svm train/model/colors_$concept.model
 done
